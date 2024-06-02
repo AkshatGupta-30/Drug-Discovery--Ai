306ae060488d51c98ba6290302cd9c014caa8dd9
AkshatGupta-30 <akshatgupta0030@gmail.com>
2024-06-02 20:12:45 +0530

    feat: Added data_management.py for loading and saving datasets/models

---
+++ drug_generator/preprocessing/data_management.py
@@ -0,0 +1,20 @@
+import os
+import pandas as pd
+import tensorflow as tf
+from drug_generator.config import config
+
+def load_dataset(file_name):
+    file_path = os.path.join(config.DATAPATH,file_name)
+    data=pd.read_csv(file_path)
+    return data
+
+
+def save_model(model_to_save):
+    save_path = os.path.join(config.SAVED_MODEL_PATH, config.MODEL_FILE_NAME)
+    model_to_save.save(save_path)
+    print("Model saved at", save_path)
+
+def load_nn_model(model_to_load):
+    save_path = os.path.join(config.SAVED_MODEL_PATH,model_to_load)
+    pretrained_model = tf.keras.models.load_model(save_path)
+    return pretrained_model
